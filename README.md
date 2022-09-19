8155cf7f8421e2650e3d4919892b4d12f6ca2928
Cristina <crispitipina@gmail.com>
2022-09-19 17:12:25 +0200

    prettier applied to all files

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 # Oh my bill
 
-
 ## Overview
 
 ![final project overview](https://img.freepik.com/vector-gratis/mensaje-construccion-simbolo-megafono_1017-31448.jpg?w=1800&t=st=1663078184~exp=1663078784~hmac=d12c98e0e0e6787009fd877612cb07b9da3b23667aff2f09b148cca5d65be1d9)
-
 
 ## The challenge
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-
 ### Links
 
 - Solution [here](https://oh-my-bill.netlify.app/)
-
 
 ### Built with
 
@@ -27,9 +23,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-
 ## Author
 
 - Website - [www.cristina-padilla.com](https://www.cristina-padilla.com)
 - Twitter - [@crispitipina](https://www.twitter.com/crispitipina)
-
--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-
-
 let hamburger = document.getElementById("hamburger");
 let sidebar = document.querySelector(".sidebar");
 
-
-hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("active"); 
-    sidebar.classList.toggle("active"); 
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  sidebar.classList.toggle("active");
 });
-
-
-
 
 /* SLIDER */
 
-
-function showEstimationSection(){
-    secondPoint.classList.add("active");
-    firstPoint.classList.add("active");
-    counterSection.style.display = "none";
-    estimationSection.style.display = "block";
+function showEstimationSection() {
+  secondPoint.classList.add("active");
+  firstPoint.classList.add("active");
+  counterSection.style.display = "none";
+  estimationSection.style.display = "block";
 }
 
-function showCounterSection(){
-    firstPoint.classList.toggle("active");
-    secondPoint.classList.remove("active");
-    counterSection.style.display = "flex";
-    estimationSection.style.display = "none";
+function showCounterSection() {
+  firstPoint.classList.toggle("active");
+  secondPoint.classList.remove("active");
+  counterSection.style.display = "flex";
+  estimationSection.style.display = "none";
 }
-
-
 
 let firstPoint = document.querySelector(".point1");
 let secondPoint = document.querySelector(".point2");
-let counterSection = document.querySelector(".counter")
-let estimationSection = document.querySelector(".estimations")
-
+let counterSection = document.querySelector(".counter");
+let estimationSection = document.querySelector(".estimations");
 
 secondPoint.addEventListener("click", showEstimationSection);
 firstPoint.addEventListener("click", showCounterSection);
